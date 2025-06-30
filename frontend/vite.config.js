0d29e4ed69d785fbacfef9f499285493a8a3f28b
kaushikbanik <166904630+kaushikbanik505@users.noreply.github.com>
2025-07-01 02:09:37 +0530

    Prepare frontend for Vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [ react() ],
+//   server: {
+//     headers: {
+//       "Cross-Origin-Embedder-Policy": "require-corp",
+//       "Cross-Origin-Opener-Policy": "same-origin"
+//     },
+//     proxy: {
+//       '/cdn': {
+//         target: 'https://unpkg.com',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/cdn/, '')
+//       }
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ react() ],
-  server: {
-    headers: {
-      "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin"
-    },
-    proxy: {
-      '/cdn': {
-        target: 'https://unpkg.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/cdn/, '')
-      }
-    }
-  }
+  plugins: [react()],
 })
